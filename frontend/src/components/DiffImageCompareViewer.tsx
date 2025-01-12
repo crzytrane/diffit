c5ba4517cc28b682565627a945fc0a72f15a8617
Mark Hamilton <crzytrane@gmail.com>
2025-01-12 20:03:28 +1300

    Diff buttons

--- frontend/src/components/DiffImageCompareViewer.tsx
+++ frontend/src/components/DiffImageCompareViewer.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import styles from './DiffImageCompareViewer.module.css';
+
 type DiffImageCompareViewerProps = {
     diffImageSrc: string;
     baseImageSrc: string;
     otherImageSrc: string;
 }
 
+type Showing = 'diff' | 'base' | 'other' | 'base-diff';
+
 export function DiffImageCompareViewer({ diffImageSrc, baseImageSrc, otherImageSrc }: DiffImageCompareViewerProps) {
-    return <div>
-        <img src={diffImageSrc} alt="Diff"></img>
-        <img src={baseImageSrc} alt="Base image to compare" />
-        <img src={otherImageSrc} alt="Other image to compare" />
-    </div>
+    const [showing, setShowing] = useState<Showing>("base-diff");
+    return <>
+        <button type="button" onClick={() => setShowing("base")} className={styles.baseButton}>Base</button>
+        <button type="button" onClick={() => setShowing("base-diff")} className={styles.diffBaseButton}>Overlay</button>
+        <button type="button" onClick={() => setShowing("other")} className={styles.otherButton}>Other</button>
+        <div style={{ display: 'grid', gridTemplateAreas: '"overlap"' }}>
+            {showing.includes('diff') && <img style={{ gridArea: 'overlap' }} src={diffImageSrc} alt="Diff" />}
+            {showing.includes('base') && <img style={{ gridArea: 'overlap' }} src={baseImageSrc} alt="Base image to compare" />}
+            {showing.includes('other') && <img style={{ gridArea: 'overlap' }} src={otherImageSrc} alt="Other image to compare" />}
+        </div></>
 }
